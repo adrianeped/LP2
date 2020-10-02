0997734fed03c8566e6b8f33af42b2d8e523d249
adrianeped <59940348+adrianeped@users.noreply.github.com>
2020-10-01 23:05:55 -0300

    add Atividade 3

---
+++ Atividade_PesoIdeal/Atividade_PesoIdeal/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Atividade_PesoIdeal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtPesoAtual = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.rdbHomem = new System.Windows.Forms.RadioButton();
+            this.rdbMulher = new System.Windows.Forms.RadioButton();
+            this.grbSexo = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.grbSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.White;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(98, 251);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(168, 41);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(33, 82);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(113, 25);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "Peso Atual:";
+            // 
+            // txtPesoAtual
+            // 
+            this.txtPesoAtual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesoAtual.Location = new System.Drawing.Point(171, 79);
+            this.txtPesoAtual.Name = "txtPesoAtual";
+            this.txtPesoAtual.Size = new System.Drawing.Size(143, 30);
+            this.txtPesoAtual.TabIndex = 2;
+            this.txtPesoAtual.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Location = new System.Drawing.Point(171, 147);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(143, 30);
+            this.txtAltura.TabIndex = 5;
+            this.txtAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(33, 153);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(69, 25);
+            this.lblAltura.TabIndex = 7;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // rdbHomem
+            // 
+            this.rdbHomem.AutoSize = true;
+            this.rdbHomem.Location = new System.Drawing.Point(44, 94);
+            this.rdbHomem.Name = "rdbHomem";
+            this.rdbHomem.Size = new System.Drawing.Size(101, 29);
+            this.rdbHomem.TabIndex = 8;
+            this.rdbHomem.Text = "Homem";
+            this.rdbHomem.UseVisualStyleBackColor = true;
+            // 
+            // rdbMulher
+            // 
+            this.rdbMulher.AutoSize = true;
+            this.rdbMulher.Checked = true;
+            this.rdbMulher.Location = new System.Drawing.Point(44, 44);
+            this.rdbMulher.Name = "rdbMulher";
+            this.rdbMulher.Size = new System.Drawing.Size(93, 29);
+            this.rdbMulher.TabIndex = 9;
+            this.rdbMulher.TabStop = true;
+            this.rdbMulher.Text = "Mulher";
+            this.rdbMulher.UseVisualStyleBackColor = true;
+            // 
+            // grbSexo
+            // 
+            this.grbSexo.Controls.Add(this.rdbMulher);
+            this.grbSexo.Controls.Add(this.rdbHomem);
+            this.grbSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbSexo.Location = new System.Drawing.Point(357, 53);
+            this.grbSexo.Name = "grbSexo";
+            this.grbSexo.Size = new System.Drawing.Size(200, 144);
+            this.grbSexo.TabIndex = 10;
+            this.grbSexo.TabStop = false;
+            this.grbSexo.Text = "Sexo:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(354, 251);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(110, 41);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.White;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(470, 251);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(110, 41);
+            this.btnSair.TabIndex = 12;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(608, 342);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.grbSexo);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPesoAtual);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.btnCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calcula Peso Ideal";
+            this.grbSexo.ResumeLayout(false);
+            this.grbSexo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtPesoAtual;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.RadioButton rdbHomem;
+        private System.Windows.Forms.RadioButton rdbMulher;
+        private System.Windows.Forms.GroupBox grbSexo;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Atividade_PesoIdeal/Atividade_PesoIdeal/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_PesoIdeal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtPesoAtual.Focus();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtPesoAtual.Clear();
+
+            txtPesoAtual.Focus();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double peso, altura, pesoIdeal;
+
+            double.TryParse(txtAltura.Text, out altura);
+            double.TryParse(txtPesoAtual.Text, out peso);
+
+            if (rdbMulher.Checked == true)
+            {
+                pesoIdeal = (62.1 * altura) - 44.7;
+            }
+            else
+            {
+                pesoIdeal = (72.7 * altura) - 58;
+            }
+
+            if (pesoIdeal > peso)
+            {
+                MessageBox.Show("Seu peso ideal é: "+ pesoIdeal + "\n" + "Coma bastante massas e doces!!");
+            }
+            else if (pesoIdeal < peso)
+            {
+                MessageBox.Show("Seu peso ideal é: " + pesoIdeal + "\n" + "Regime obrigatório já!!");
+            }
+            else
+            {
+                MessageBox.Show("Seu peso ideal é: " + pesoIdeal + "\n" + "Você está com o peso ideal!!");
+            }
+
+        }
+    }
+}
